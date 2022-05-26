068ae11e0fff362920937e520e3d151a57c1bdf3
Erik-Handeland <erikhandeland@gmail.com>
2022-05-25 22:58:41 -0700

    Update index.js

--- mySQL/src/api/index.js
+++ mySQL/src/api/index.js
@@ -5,5 +5,7 @@ router.use('/locations', require('./locations'))
 router.use('/parkingDatas', require('./parkingDatas'))
 router.use('/counties', require('./counties'))
 router.use('/recAreas', require('./recAreas'))
+router.use('/images', require('./images'))
+router.use('/users', require('./users'))
 
 module.exports = router
