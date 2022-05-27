9d580c3051ed142fe6f224d0ac3c1b4da5cf2a0a
Erik Handeland <erikhandeland@gmail.com>
2022-05-27 09:40:48 -0700

    Delete src directory

--- src/api/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-const router = Router()
-
-router.use('/locations', require('./locations'))
-router.use('/parkingDatas', require('./parkingDatas'))
-router.use('/images', require('./images'))
-router.use('/users', require('./users'))
-
-module.exports = router
